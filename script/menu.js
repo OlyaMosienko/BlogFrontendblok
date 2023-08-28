f436da81181be5b6750f95e93bd2cf268943ec24
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-08-28 19:18:13 +0300

    Add modal menu

---
+++ script/menu.js
@@ -0,0 +1,19 @@
+// Overlay Menu
+
+const burger = document.getElementById("burger");
+const menu = document.getElementById("overlayMenu");
+const main = document.querySelector(".main");
+
+// Sidebar Menu Toggle
+burger.addEventListener("click", function () {
+	burger.classList.toggle("active");
+	menu.classList.toggle("active");
+});
+
+// Close Sidebar by Click Outside
+main.addEventListener("click", function () {
+	if (menu.classList.contains("active")) {
+		burger.classList.remove("active");
+		menu.classList.remove("active");
+	}
+});
