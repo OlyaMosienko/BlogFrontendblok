77d6ef81c647bdbf9a89efd7fe726b7f4683d7e2
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-08-29 18:24:52 +0300

    Add modals in over pages

--- script/menu.js
+++ script/menu.js
@@ -1,5 +1,4 @@
 // Burger Menu
-
 let btn = document.querySelector('.burger-btn');
 
 btn.addEventListener('click', function () {
@@ -7,7 +6,6 @@ btn.addEventListener('click', function () {
 });
 
 // Overlay Menu
-
 const burger = document.getElementById("burger");
 const menu = document.getElementById("overlayMenu");
 const main = document.querySelector(".main");
@@ -23,5 +21,32 @@ main.addEventListener("click", function () {
 	if (menu.classList.contains("active")) {
 		burger.classList.remove("active");
 		menu.classList.remove("active");
+	}
+});
+
+// Search
+const searchBtn = document.getElementById("searchBtn");
+const search = document.getElementById("overlay");
+const searchClose = document.getElementById("searchClose");
+
+// Sidebar Search Toggle
+searchBtn.addEventListener("click", function () {
+	searchBtn.classList.toggle("active");
+	search.classList.toggle("active");
+});
+
+// Close Sidebar Search by Click Close Btn
+searchClose.addEventListener("click", function () {
+	if (search.classList.contains("active")) {
+		searchBtn.classList.remove("active");
+		search.classList.remove("active");
+	}
+});
+
+// Close Sidebar Search by Click Outside
+main.addEventListener("click", function () {
+	if (search.classList.contains("active")) {
+		searchBtn.classList.remove("active");
+		search.classList.remove("active");
 	}
 });
