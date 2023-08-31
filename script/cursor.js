1efe0227902db4106810422c33b1a8ab1eaaaba7
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-08-31 17:47:19 +0300

    Add cursor to all pages

--- script/cursor.js
+++ script/cursor.js
@@ -1,13 +1,41 @@
-let customCursor = document.querySelector('.cursor');
+//let customCursor = document.querySelector('.cursor');
 
-document.addEventListener('mousemove', moveCursor);
+//document.addEventListener('mousemove', moveCursor);
+//cursor.style.boxShadow = '0 5px 15px rgba(18, 192, 221, 1)';
 
-function moveCursor(e){
-    let x = e.clientX;
-    let y = e.clientY;
+//function moveCursor(e){
+    //let x = e.clientX;
+    //let y = e.clientY;
 
-    customCursor.style.left = `${x}px`;
-    customCursor.style.top = `${y}px`;
+    //customCursor.style.left = `${x}px`;
+    //customCursor.style.top = `${y}px`;
 
-    this.speed = 0.5;
-}
+    //this.speed = 0.5;
+//}
+
+// Создаем div элемент для кастомного курсора
+var cursor = document.createElement("div");
+cursor.className = "custom-cursor";
+document.body.appendChild(cursor);
+
+// Перемещаем кастомный курсор вместе с мышью
+document.addEventListener("mousemove", function(event) {
+  var x = event.clientX;
+  var y = event.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+  this.speed = 0.5;
+});
+
+// Скрытие стандартного курсора при наведении на элементы
+var elementsWithCustomCursor = document.querySelectorAll(".custom-cursor-element");
+
+elementsWithCustomCursor.forEach(function(element) {
+  element.addEventListener("mouseenter", function() {
+    cursor.style.display = "none";
+  });
+  
+  element.addEventListener("mouseleave", function() {
+    cursor.style.display = "block";
+  });
+});
