35f1941d3ffa5dfb10c269b8697abb417517032b
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-09-20 15:34:57 +0300

    Add changes in transition, modal menu and max-width of content

--- script/menu.js
+++ script/menu.js
@@ -9,18 +9,21 @@ btn.addEventListener('click', function () {
 const burger = document.getElementById("burger");
 const menu = document.getElementById("overlayMenu");
 const main = document.querySelector(".main");
+const body = document.querySelector("body");
 
 // Sidebar Menu Toggle
 burger.addEventListener("click", function () {
 	burger.classList.toggle("active");
 	menu.classList.toggle("active");
+	body.classList.toggle("_opened-menu");
 });
 
-// Close Sidebar by Click Outside
+// Close Sidebar by Click Btn
 main.addEventListener("click", function () {
 	if (menu.classList.contains("active")) {
 		burger.classList.remove("active");
 		menu.classList.remove("active");
+		body.classList.remove("_opened-menu");
 	}
 });
 
