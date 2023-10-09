ca4b526cb4224f476c28b26c791d705de54382d2
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-10-09 19:34:24 +0300

    Add changes in script and some changes in page Post

--- script/main.js
+++ script/main.js
@@ -24,3 +24,53 @@ var swiper = new Swiper(".recommend-swiper", {
         }
       }
 });
+
+
+// Overlay Menu
+const btn = document.querySelector('.burger-btn');
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#overlayMenu");
+const main = document.querySelector(".main");
+const body = document.querySelector("body");
+
+// Sidebar Menu Open/Close
+burger.addEventListener("click", function () {
+  if (menu.classList.contains("active")) {
+    btn.classList.remove('is-active');
+    menu.classList.remove("active");
+		burger.classList.remove("active");
+		body.classList.remove("_opened-menu");
+	}else{
+    btn.classList.add('is-active');
+    burger.classList.add("active");
+    menu.classList.add("active");
+    body.classList.add("_opened-menu");
+  }
+});
+
+// Search
+const searchBtn = document.querySelector("#searchBtn");
+const search = document.querySelector("#overlay");
+const searchClose = document.querySelector("#searchClose");
+
+// Sidebar Search Toggle
+searchBtn.addEventListener("click", function () {
+	searchBtn.classList.add("active");
+	search.classList.add("active");
+});
+
+// Close Sidebar Search by Click Close Btn
+searchClose.addEventListener("click", function () {
+	if (search.classList.contains("active")) {
+		searchBtn.classList.remove("active");
+		search.classList.remove("active");
+	}
+});
+
+// Close Sidebar Search by Click Outside
+main.addEventListener("click", function () {
+	if (search.classList.contains("active")) {
+		searchBtn.classList.remove("active");
+		search.classList.remove("active");
+	}
+});
